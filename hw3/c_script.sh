001549e57045dcd7326076d1f0a525e5f98e3de2
Vishal Srivastav <visr2941@colorado.edu>
2017-02-05 19:04:22 -0700

    hw3 added

---
+++ hw3/c_script.sh
@@ -0,0 +1,6 @@
+echo "#include <stdio.h>" > $1
+echo "#include <stdlib.h>" >> $1
+echo "#include <stdint.h>" >> $1
+echo -e "#include <string.h>\n" >> $1
+echo -e "void main()" >> $1
+echo -e "{\n\n}" >> $1
---
+++ hw3/visr2941_hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main()
+{
+	printf("Vishal Srivastav Hello World\n");
+}
