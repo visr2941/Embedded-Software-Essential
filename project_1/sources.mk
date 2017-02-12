eabe42e1a15169f05149d92fe57c154b120ab013
Vishal Srivastav <visr2941@colorado.edu>
2017-02-12 06:22:05 -0700

    first draft of working makefile

--- project_1/sources.mk
+++ project_1/sources.mk
@@ -1,4 +1,4 @@
-EXE = project1.out
-SOURCE := src/data.c src/memory.c
-OBJECT := libraries/data.o libraries/memory.o
-INCLUDE = inc/
+EXE = project1
+SOURCE := src/project_1.c src/data.c src/memory.c
+OBJECT := libraries/data.o libraries/memory.o libraries/project_1.o
+INCLUDE = -Iinc/
