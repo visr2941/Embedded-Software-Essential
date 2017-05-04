73763ca00acdfddc4093f776506e670e2a8798e5
Vishal Srivastav <visr2941@colorado.edu>
2017-05-04 02:10:25 -0600

    project 4 added

---
+++ project_4_part_1/Sources/main.h
@@ -0,0 +1,23 @@
+/*
+ * main.h
+ *
+ *  Created on: Apr 8, 2017
+ *      Author: vishal
+ */
+
+#ifndef SOURCES_MAIN_H_
+#define SOURCES_MAIN_H_
+
+#include "cirbuf.h"
+#include "control_layer.h"
+#include "data.h"
+#include "memory.h"
+#include "uart.h"
+#include "MKL25Z4.h"
+#include "string.h"
+//#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include "rtc.h"
+
+#endif /* SOURCES_MAIN_H_ */
